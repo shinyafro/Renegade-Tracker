c1846d97eefb501c9d31a57ae0251b20fad37af0
Shiny <ShinyTourettesX@gmail.com>
2021-03-10 20:18:55 +0800

    register the commands

--- src/main/java/renegade/planetside2/command/DiscordCommandManager.java
+++ src/main/java/renegade/planetside2/command/DiscordCommandManager.java
@@ -4,7 +4,11 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
 import renegade.planetside2.RenegadeTracker;
+import renegade.planetside2.command.ps2.CheckCamo;
+import renegade.planetside2.command.ps2.CheckUser;
+import renegade.planetside2.command.ps2.Unlink;
 import renegade.planetside2.exception.DiscordCommandException;
+import renegade.planetside2.tracker.Rank;
 import renegade.planetside2.util.Utility;
 
 import java.util.ArrayList;
@@ -16,7 +20,29 @@ public class DiscordCommandManager extends DiscordCommandTree {
     private final HashSet<String> defaultAliases = new HashSet<>(Arrays.asList("", "help"));
 
     public DiscordCommandManager() {
+        DiscordCommand checkCamo = DiscordCommand.builder()
+                .setRequiredRanks(Rank.OFFICER)
+                .setDescription("Runs the scheduled rank/camo check early.")
+                .setCommandExecutor(new CheckCamo())
+                .build();
 
+        DiscordCommand unlink = DiscordCommand.builder()
+                .setRequiredRanks(Rank.OFFICER)
+                .setDescription("Unlinks a user.")
+                .setCommandUsage("<discord/planetside> <name/id/@tag>")
+                .setCommandExecutor(new Unlink())
+                .build();
+
+        DiscordCommand checkUser = DiscordCommand.builder()
+                .setRequiredRanks(Rank.OFFICER)
+                .setDescription("checks a linked user for camo.")
+                .setCommandUsage("<discordId/@tag>")
+                .setCommandExecutor(new CheckUser())
+                .build();
+
+        register(checkCamo, "check-camo");
+        register(unlink, "unlink");
+        register(checkUser, "check-user");
     }
 
     public void process(TextChannel source, Member author, String args){
