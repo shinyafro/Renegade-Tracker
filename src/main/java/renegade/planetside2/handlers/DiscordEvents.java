ab6eb4a6fe403ebd6a19f7fabc0c632e0c5cb1f2
Shiny <ShinyTourettesX@gmail.com>
2021-03-11 00:49:55 +0800

    various tweaks

--- src/main/java/renegade/planetside2/handlers/DiscordEvents.java
+++ src/main/java/renegade/planetside2/handlers/DiscordEvents.java
@@ -1,5 +1,7 @@
 package renegade.planetside2.handlers;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
@@ -10,6 +12,7 @@ import renegade.planetside2.RenegadeTracker;
 import renegade.planetside2.command.DiscordCommandManager;
 import renegade.planetside2.storage.Configuration;
 import renegade.planetside2.tracker.UserManager;
+import renegade.planetside2.util.Utility;
 
 import static renegade.planetside2.util.Utility.embed;
 
@@ -24,9 +27,16 @@ public class DiscordEvents {
     public void onMemberJoin(GuildMemberJoinEvent event){
         Configuration cfg = RenegadeTracker.INSTANCE.getConfig();
         if (event.getUser().isBot() || !cfg.sendJoinMessage()) return;
+        MessageEmbed embed = Utility.embed()
+                .addField("Welcome", "Welcome to the official Renegade-18 discord server.\n" +
+                        "To access the rest of the server, please click <#695926293426077717>\n" +
+                        "for further instructions.", false)
+                .addField("If you are a member", "Please also type your in-game name below.", false)
+                .addField("If you need any help", "Please contact any of the following admins:\n" +
+                        "<@248056002274918400>, <@178370119213252608>, <@272173309850943488>", false)
+                .build();
         event.getUser().openPrivateChannel()
-                .flatMap(ch->ch.sendMessage(embed("Welcome!", "Welcome to renegade 18. Please input your in-game name.\n" +
-                        "If you are not a member you are not required to do this.")))
+                .flatMap(ch->ch.sendMessage(embed))
                 .queue();
     }
 
