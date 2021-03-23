e364e73d027f9dc3506a06ba6035469b2c203463
Shiny <ShinyTourettesX@gmail.com>
2021-03-23 16:36:10 +0800

    Making linking channel / DM the same thing

--- src/main/java/renegade/planetside2/handlers/DiscordEvents.java
+++ src/main/java/renegade/planetside2/handlers/DiscordEvents.java
@@ -46,12 +46,11 @@ public class DiscordEvents {
     @SubscribeEvent
     public void onPrivateMessage(PrivateMessageReceivedEvent event){
         if (event.getAuthor().isBot()) return;
-        String username = event.getMessage()
+        String usernameRaw = event.getMessage()
                 .getContentRaw()
                 .trim();
-        username = username.contains(" ")? username.split(" ")[0]: username;
         User user = event.getAuthor();
-        manager.linkAccount(user, user, username);
+        parseLinkRequest(usernameRaw, user);
     }
 
     @SubscribeEvent
@@ -64,23 +63,7 @@ public class DiscordEvents {
                     .getContentRaw()
                     .trim();
             User user = event.getAuthor();
-            conf.getGuild(RenegadeTracker.INSTANCE.getJda())
-                    .retrieveMember(user)
-                    .submit()
-                    .thenAccept(member->{
-                        boolean officer = member.getRoles().contains(conf.getRole(Rank.OFFICER));
-                        String[] parts = usernameRaw.contains(" ")? usernameRaw.split(" "): null;
-                        if (officer && parts != null && parts[0].matches("<@!\\d+>")) {
-                            String tag = parts[0].replaceAll("<@!(\\d+)>", "$1");
-                            long id = Long.parseLong(tag);
-                            String username = parts.length > 1? parts[1]: "N/A";
-                            RenegadeTracker.INSTANCE.getJda().retrieveUserById(id).submit()
-                                    .thenAccept(target->manager.linkAccount(user, target, username));
-                        } else {
-                            String username = parts != null? parts[0]: usernameRaw;
-                            manager.linkAccount(user, user, username);
-                        }
-                    });
+            parseLinkRequest(usernameRaw, user);
         } else if (channel == conf.getCommandChannelId()) {
             final String pre = conf.getCommandPrefix();
             final String message = event.getMessage().getContentRaw();
@@ -90,5 +73,27 @@ public class DiscordEvents {
             event.getGuild().retrieveMember(event.getAuthor()).submit()
                     .thenAccept(m->commandBase.process(event.getChannel(), m, args));
         }
+    }
+
+    public void parseLinkRequest(String usernameRaw, User user) {
+        Configuration conf = RenegadeTracker.INSTANCE.getConfig();
+        conf.getGuild(RenegadeTracker.INSTANCE.getJda())
+                .retrieveMember(user)
+                .submit()
+                .thenAccept(member->{
+                    boolean officer = member.getRoles().contains(conf.getRole(Rank.OFFICER));
+                    String[] parts = usernameRaw.contains(" ")? usernameRaw.split(" "): null;
+                    if (officer && parts != null && parts[0].matches("<@!\\d+>")) {
+                        String tag = parts[0].replaceAll("<@!(\\d+)>", "$1");
+                        long id = Long.parseLong(tag);
+                        String username = parts.length > 1? parts[1]: "N/A";
+                        RenegadeTracker.INSTANCE.getJda().retrieveUserById(id).submit()
+                                .thenAccept(target->manager.linkAccount(user, target, username));
+                    } else {
+                        String username = parts != null? parts[0]: usernameRaw;
+                        manager.linkAccount(user, user, username);
+                    }
+                });
+
     }
 }
